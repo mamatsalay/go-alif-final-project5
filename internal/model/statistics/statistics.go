1731d6109dfc7492398b1fd2e51045e5bfc107ac
mamatsalay <mamatsalayy@icloud.com>
2025-05-19 21:49:53 +0500

    models and linter added

--- internal/model/statistics/statistics.go
+++ internal/model/statistics/statistics.go
@@ -1,1 +1,9 @@
 package statistics
+
+type WorkoutStatistics struct {
+	Category    string  `json:"category"`
+	TotalWeight float64 `json:"total_weight"`
+	UserID      int     `json:"user_id"`
+	TotalSets   int     `json:"total_sets"`
+	TotalReps   int     `json:"total_reps"`
+}
--- internal/model/user/user.go
+++ internal/model/user/user.go
@@ -1,1 +1,17 @@
 package user
+
+import "time"
+
+type Role string
+
+const AdminRole = Role("admin")
+const UserRole = Role("user")
+
+type User struct {
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Username  string    `json:"username"`
+	Password  string    `json:"password"`
+	Role      Role      `json:"role"`
+	ID        int       `json:"id"`
+}
